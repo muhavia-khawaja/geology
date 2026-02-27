f93252ea9ca1c98c667c79e7d3d1b7f2d2cbc0b2
super-website <kh.muhavia1@gmail.com>
2026-02-27 20:11:25 +0500

    Initial commit

--- app/(frontend)/real-time-data/page.tsx
+++ app/(frontend)/real-time-data/page.tsx
@@ -1,6 +1,19 @@
-import EarthQuakeMap from '@/components/EarthquakeMapComponent'
-import React from 'react'
+import dynamic from 'next/dynamic'
 
-export default function page() {
-  return <EarthQuakeMap />
+const EarthquakeMapContent = dynamic(
+  () => import('@/components/EarthquakeMapComponent'),
+  {
+    ssr: false,
+    loading: () => (
+      <p className='text-white text-center p-10'>Loading Map...</p>
+    ),
+  },
+)
+
+export default function RealTimeDataPage() {
+  return (
+    <main>
+      <EarthquakeMapContent />
+    </main>
+  )
 }
